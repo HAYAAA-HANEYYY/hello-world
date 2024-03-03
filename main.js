dd9a72c9ac30d54ffdfe56dfc4c85ba2577bd00e
HAYAAA-HANEYYY <160444823+HAYAAA-HANEYYY@users.noreply.github.com>
2024-03-03 11:37:43 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+"use strict";
+console.log("hello world ");
+console.log("thanks kamran tessori");
+console.log("governor sindh");
+console.log("3 class");
---
+++ main.ts
@@ -0,0 +1,7 @@
+console.log("hello world ");
+console.log("thanks kamran tessori");
+console.log("governor sindh");
+console.log("3 class");
+
+
+
